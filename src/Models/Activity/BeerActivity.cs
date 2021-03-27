9808f040bf97af52d92c3e3eceda6c31055a635a
MolinRE <newomeg@gmail.com>
2021-03-27 12:57:11 +0300

    Added venue activty

--- src/Models/Activity/BeerActivity.cs
+++ src/Models/Activity/BeerActivity.cs
@@ -31,4 +31,19 @@ namespace Saison.Models.Activity
         [JsonPropertyName("checkins")]
         public Checkins Checkins { get; set; }
     }
+    
+    public class VenueActivity
+    {
+        [JsonPropertyName("type")]
+        public string Type { get; set; }
+
+        [JsonPropertyName("pagination")]
+        public Pagination Pagination { get; set; }
+
+        [JsonPropertyName("mem")]
+        public bool Mem { get; set; }
+
+        [JsonPropertyName("checkins")]
+        public Checkins Checkins { get; set; }
+    }
 }
