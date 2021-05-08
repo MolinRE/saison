589b2ae944a64cea2f89f650b1ac89f58641d4ae
MolinRE <newomeg@gmail.com>
2021-05-08 23:57:02 +0300

    Testing GitHub Actions

---
+++ src/Models/Beer/CollaborationsWith.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Text.Json.Serialization;
+
+namespace Saison.Models.Beer
+{
+    public class CollaborationsWith
+    {
+        [JsonPropertyName("count")]
+        public int Count { get; set; }
+
+        [JsonPropertyName("items")]
+        public List<CollaborationBreweryContainer> Items { get; set; }
+    }
+}
